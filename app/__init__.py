509494b12e29b67b3e0d2f7f839e1b77e6212193
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 17:53:34 +0300

    This commit configure Uploadset

--- app/__init__.py
+++ app/__init__.py
@@ -39,4 +39,10 @@ def create_app(config_name):
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
     
