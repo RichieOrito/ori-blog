0f212d712db8df45c7719f9b86962e1f5e44e276
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 22:50:52 +0300

    This commit creates a function that registers users

--- app/auth/views.py
+++ app/auth/views.py
@@ -22,3 +22,20 @@ def login():
 
     title = "Blog|Login"
     return render_template('auth/login.html',login_form = login_form,title=title)
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    """
+    Function that registers the users
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,firstname= form.firstname.data,lastname= form.lastname.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Blog Quote","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
