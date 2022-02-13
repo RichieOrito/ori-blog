7f1da70cc070b5607b6cc980843746e1fc62b8cf
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 17:52:51 +0300

    This commit registers the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -31,4 +31,12 @@ def create_app(config_name):
     db.init_app(app)
     
     mail.init_app(app)
+
+    # Registering the blueprint
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
     
