094231c42d479788ff07870a5cee72b0ae32d105
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 21:57:48 +0300

    This commit creates Class to create a wtf form for creating a blog and

---
+++ app/main/forms.py
@@ -0,0 +1,18 @@
+from wtforms.validators import Required
+from wtforms import StringField, TextAreaField, SubmitField, SelectField
+from flask_wtf import FlaskForm
+
+class BlogForm(FlaskForm):
+    """
+    Class to create a wtf form for creating a blog
+    """
+    title_blog = StringField('Title')
+    description = TextAreaField('Write a Description', validators=[Required()])
+    submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    """
+    Class to create a wtf form for creating a pitch
+    """
+    comment = TextAreaField('Comment')
+    submit = SubmitField('Submit')
