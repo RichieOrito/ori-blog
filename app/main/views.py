202838f3b229745437cc0e5812d0e83568608883
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 20:42:38 +0300

    This commit creates a view blog function that returns the Blog page and data

--- app/main/views.py
+++ app/main/views.py
@@ -25,3 +25,21 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/blog/new', methods=['GET', 'POST'])
+@login_required
+def blogs():
+    """
+    View Blog function that returns the BLog page and data
+    """
+    blog_form = BlogForm()
+    if blog_form.validate_on_submit():
+        title_blog= blog_form.title_blog.data
+        description = blog_form.description.data
+        new_blog = Blog(title_blog=title_blog, description=description, user=current_user)
+        db.session.add(new_blog)
+        db.session.commit()
+        return redirect(url_for('main.theblog'))
+    title = 'My Blog'
+    return render_template('blogs.html', title=title, blog_form=blog_form)
+
