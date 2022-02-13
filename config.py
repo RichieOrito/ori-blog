4567815e533f5e667400bd115f81b22092855ac6
RichieOrito <richard.omondi@student.moringaschool.com>
2022-02-13 13:48:34 +0300

    this commit adds secret key and email configurations and also the simple mde configurations

--- config.py
+++ config.py
@@ -1,27 +1,25 @@
+import os
+
 class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
 
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-class ProdConfig(Config):
-    '''
-    Production  configuration child class
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'Pitch'
+    SENDER_EMAIL = 'richard.omondi@student.moringaschool.com'
 
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
 
-
-class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
-    DEBUG = True
+    @staticmethod
+    def init_app(app):
+        pass
